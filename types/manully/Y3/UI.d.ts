509f408ca4878af3980ca09b23e1deb00a42db22
Icarus <2654808955@qq.com>
2023-08-14 22:06:55 +0800

    !R 后缀错了

---
+++ types/manully/Y3/UI.d.ts
@@ -0,0 +1,5 @@
+﻿declare module y3.ui
+{
+    function create_ui(this:void,player: Player, parent_ui: UI, comp_type: y3.Const.UIComponentType): UI;
+    function get_ui(this:void,player: Player, ui_path: string): UI;   
+}
--- types/manully/Y3/UI.ts
+++
@@ -1,5 +0,0 @@
-﻿declare module y3.ui
-{
-    function create_ui(this:void,player: any, parent_ui: any, comp_type: any): UI;
-    function get_ui(this:void,player: any, ui_path: any): UI;   
-}
