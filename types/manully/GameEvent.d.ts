b19a8f9a7a2f7841d9942fbd8d2e52d469d8c7d3
Icarus <2654808955@qq.com>
2023-08-15 21:10:04 +0800

    !RA 事件准备都转换为`GlobalEvent`

--- types/manully/GameEvent.d.ts
+++ types/manully/GameEvent.d.ts
@@ -2,4 +2,8 @@
 declare interface Game
 {
     event(name:y3.Const.GlobalEventType, act:ActionArgsNoSelf):Trigger;
+
+    event<TEName extends keyof GlobalEvent>(name: TEName, act:Action1NoSelf<GlobalEvent[TEName]>):Trigger;
+
+    event<TEName extends keyof GlobalEvent>(name: TEName, type:string, act:Action1NoSelf<GlobalEvent[TEName]>):Trigger;
 }
