0283a8e5efc9ed7331b378cd61eae0f657652fa8
Icarus <1375400884@qq.com>
2023-08-16 02:45:12 +0800

    !B 不小心删了=-=

--- types/manully/Y3/Y3.d.ts
+++ types/manully/Y3/Y3.d.ts
@@ -1,3 +1,36 @@
 ﻿declare module y3
 {
+    const game:Game;
+    const helper:Helper;
+    const ground:Ground;
+    const math:Math;
+    const unit:Unit;
+    const ability:Ability;
+    const destructible:Destructible;
+    const item:Item;
+    const buff:Buff;
+    const projectile:Projectile;
+    const technology:Technology;
+    const beam:Beam;
+
+    const item_group:ItemGroup;
+    const mover:Projectile;
+    const particle:Particle;
+    const player:Player;
+    const player_group:PlayerGroup;
+    const unit_group:UnitGroup;
+    const projectile_group:ProjectileGroup;
+    const selector:Selector;
+    const cast:Cast;
+    const damage_instance:DamageInstance;
+    const sound:Sound;
+
+    const area:Area;
+    const camera:Camera;
+    const light:Light;
+    const road:Road;
+    const point:Point;
+    const scene_ui:SceneUI;
+    const ui_prefab:UIPrefab;
+    const shape:Shape;
 }
