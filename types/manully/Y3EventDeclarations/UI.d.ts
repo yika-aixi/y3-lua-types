41f0c2599ee881b486e0242c41ae77d92230f30a
Icarus <1375400884@qq.com>
2023-08-17 01:51:21 +0800

    !O(manully.Y3EventDeclarations.UI) 使用Action来取代手动声明

--- types/manully/Y3EventDeclarations/UI.d.ts
+++ types/manully/Y3EventDeclarations/UI.d.ts
@@ -1,5 +1,5 @@
 ﻿
-declare type GameEventName = 
+declare type GameEventName =
     | y3.Const.AbilityEventType
     | y3.Const.GlobalEventType
     | y3.Const.ItemEventType
@@ -15,8 +15,10 @@ declare type GameEventName =
 /** 这个接口是动作函数的参数
  * 申明于 script\y3\meta\event.lua
  * */
-declare interface GameEventDeclarations extends Record<GameEventName, object> {
-    [y3.Const.GlobalEventType.UI_EVENT]: {
+declare interface GameEventDeclarations extends Record<GameEventName, object>
+{
+    [y3.Const.GlobalEventType.UI_EVENT]:
+    {
         // 触发事件的玩家
         player: Player,
         // ui事件变量名
@@ -29,7 +31,8 @@ declare interface GameEventDeclarations extends Record<GameEventName, object> {
         touch_id: number
     }
 
-    [y3.Const.GlobalEventType.CUSTOM_EVENT]: {
+    [y3.Const.GlobalEventType.CUSTOM_EVENT]:
+    {
         c_param_1: any
         c_param_dict: any
     }
@@ -38,19 +41,19 @@ declare interface GameEventDeclarations extends Record<GameEventName, object> {
 /** 这个接口是事件的参数构成，整体结构是 事件名, ...辅助参数, 动作函数
  * 申明于 script\y3\meta\eventconfig.lua
  */
-declare interface GameEventParams extends Record<GameEventName, (...args: [...any[], GameEventCallback<GameEventName>]) => void> {
-
+declare interface GameEventParams extends Record<GameEventName, (...args: [...any[], GameEventCallback<GameEventName>]) => void>
+{
     [y3.Const.GlobalEventType.UI_EVENT]: (event_name: string, fun: GameEventCallback<y3.Const.GlobalEventType.UI_EVENT>) => void
     [y3.Const.GlobalEventType.CUSTOM_EVENT]: (event_name: number, fun: GameEventCallback<y3.Const.GlobalEventType.CUSTOM_EVENT>) => void
 
 }
 
 //@ts-nocheck
-declare type GameEventCallback<T> = (
-    this: Trigger,
-    data: T extends string
-        ? T extends keyof GameEventDeclarations
-        ? GameEventDeclarations[T]
-        : { _py_params: LuaUserdata }
-        : T,
-) => void
+declare type GameEventCallback<T> = Action1<
+    Trigger,
+    T extends string ?
+        T extends keyof GameEventDeclarations ?
+            GameEventDeclarations[T] : { _py_params: LuaUserdata }
+        :
+        T
+>
