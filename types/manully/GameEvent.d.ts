b480011928550a1dbbc0ad06e5a614ab3f775551
Icarus <1375400884@qq.com>
2023-08-18 03:48:12 +0800

    !O(manully.GameEvent) event使用事件参数定义进行事件参数列表推断

--- types/manully/GameEvent.d.ts
+++ types/manully/GameEvent.d.ts
@@ -1,14 +1,23 @@
+// //@ts-nocheck
+declare type GameEventCallback<T> = Action1<
+    Trigger,
+    T extends string ?
+        T extends keyof GameEventDeclarations ?
+            GameEventDeclarations[T] : { _py_params: LuaUserdata }
+        :
+        T
+>
 
 declare interface Game
 {
     event<T extends string | object>(
-            /** 事件名 */
-            name: (T extends string ? T : string) | GameEventName,
-            /** 事件的辅助参数 ，这里应该是 若干个， */
-            ...args: T extends string 
-            ? T extends keyof GameEventParams 
-                ? Parameters<GameEventParams[T]>
+        /** 事件名 */
+        name: (T extends string ? T : string) | GameEventNames,
+        /** 事件的辅助参数 ，这里应该是 若干个， */
+        ...args: T extends string
+            ? T extends keyof GameEventParamsDeclarations
+                ? Parameters<GameEventParamsDeclarations[T]>
                 : [GameEventCallback<any> ]
             : [GameEventCallback<T>]
-        ): Trigger;
+    ): Trigger;
 }
